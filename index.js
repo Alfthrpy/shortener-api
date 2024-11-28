ac91c8cd5b2ad831a8c1e986c90c5b2985e5cc96
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-11-28 17:40:17 +0700

    fix : prod

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import app from './src/app.js'
+import express from 'express';
+import dotenv from "dotenv";
+import userRoute from './src/route/userRoute.js';
+import shortenerRoute from './src/route/shortenerRoute.js';
+import statsRoute from './src/route/statsRoute.js';
+import authRoute from './src/route/authRoute.js';
+import { swaggerDocs, swaggerUi } from './swagger.js';
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/user',userRoute)
+app.use('/',shortenerRoute);
+app.use('/api/stats',statsRoute);
+app.use('/api/auth',authRoute)
+app.use('/docs/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+app.use('*', (req, res) => {
+    return res.json({error : "not-found"}).status(404)
 });
 
 export default app;
+
+
+
