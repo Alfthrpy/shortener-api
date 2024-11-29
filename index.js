eb562ffec3146c77468e8c6ad3cfb3f9b1e465ca
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-11-29 12:56:34 +0700

    coba

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ import shortenerRoute from './src/route/shortenerRoute.js';
 import statsRoute from './src/route/statsRoute.js';
 import authRoute from './src/route/authRoute.js';
 import { swaggerDocs, swaggerUi } from './swagger.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +21,7 @@ app.use('/', shortenerRoute);
 app.use('/api/stats', statsRoute);
 app.use('/api/auth', authRoute);
 app.use('/docs/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/docs/api-docs', express.static(path.join(__dirname, 'node_modules/swagger-ui-dist')));
 
 app.use('*', (req, res) => {
     return res.status(404).json({ error: "not-found" });
